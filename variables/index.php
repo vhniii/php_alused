cddb68108dc9fe95a7ef231bdf7f123c4fa3a774
vhniii <rasmus.laane@khk.ee>
2018-12-13 15:37:20 +0200

    Muutujate defineerimine

--- variables/index.php
+++ variables/index.php
@@ -4,3 +4,29 @@
 // Muutujate defineermine
 
 // $[muutujanimi] = [väärtus];
+
+
+$taisArv = 7;
+$reaalArv = 3.14;
+$sone = '7';
+$toevaartusTrue = true;
+$toevaartusFalse = False;
+
+
+echo 'Täisarv = '.$taisArv.'<br>';
+echo 'Reaalarv = '.$reaalArv.'<br>';
+echo 'Sõne = '.$sone.'<br>';
+echo 'Tõeväärtus true = '.$toevaartusTrue.'<br>';
+echo 'Tõeväärtus false = '.$toevaartusFalse.'<br>';
+
+
+echo 'Muutuja "sõne" andmetüüp on: '.gettype($sone).'<br>';
+$summa = $sone + $sone.'<br>';
+echo 'Vastus = '.$summa.'<br>';
+
+
+echo gettype($sone).'<br> ';
+settype($sone, 'int').'<br>';
+
+echo 'Andmetüüp on: '.gettype($sone);
+
