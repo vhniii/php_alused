065319193b62e9c123c946055bec87470086935c
vhniii <rasmus.laane@khk.ee>
2019-01-11 18:47:10 +0200

    Vormist info töötlemine - Ajafunktsioonid - Eestipärane kuupäev

--- date/index.php
+++ date/index.php
@@ -3,3 +3,15 @@
 echo 'Time = '.time().'<br>';
 echo 'Date: '.date('d.m.Y G:i' , time()).'<br>';
 echo 'Ajavöönd: '.date_default_timezone_get('Europe/Tallinn').'<br>';
+
+
+echo date('d.F.Y').'<br>';
+
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember' );
+
+
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+
+echo 'Eestipärane kuupäev: '.$paev.'.'.$kuu.'.'.$aasta.'<br>';
