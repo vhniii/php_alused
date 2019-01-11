0e0c11c152032932e5dd60624998157ea5478c6a
vhniii <rasmus.laane@khk.ee>
2019-01-11 18:46:46 +0200

    Vormist info töötlemine - Ajafunktsioonid - Ajavöönd

--- date/index.php
+++ date/index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo 'Time = '.time();
-echo 'Date: '.date('d.m.Y G:i' , time());
-echo 'Ajavöönd: '.date_default_timezone_get('Europe/Tallinn');
+echo 'Time = '.time().'<br>';
+echo 'Date: '.date('d.m.Y G:i' , time()).'<br>';
+echo 'Ajavöönd: '.date_default_timezone_get('Europe/Tallinn').'<br>';
