3d2d41332baced5ba2face49dd3d8d5903cb69b8
vhniii <rasmus.laane@khk.ee>
2019-01-11 19:19:24 +0200

    Vormist info töötlemine - Ajafunktsioonid - Kuupäeva kontrollimine

--- date/index.php
+++ date/index.php
@@ -23,4 +23,45 @@ echo 'Muu soovitud kuupäeva genereerimine: '.date('d.m.Y', $a).'<br><br>';
 
 echo '1min pärast on kell: '.date('d.m.Y G:i', time() + 60).'<br>';
 echo '1hr pärast on kell: '.date('d.m.Y G:i', time() + 60*60).'<br>';
-echo '24hr pärast on kell: '.date('d.m.Y G:i', time() + 60*60*24).'<br>';
+echo '24hr pärast on kell: '.date('d.m.Y G:i', time() + 60*60*24).'<br><br>';
+
+
+$arv1 = 10;
+$arv2 = 54;
+$arv3 = 22;
+
+$aasta = 2018;
+$aasta2 = 2017;
+
+
+if (checkdate($arv2, $arv1, $aasta)) {
+
+    echo ('Kuupäev korras!<br>');
+    
+} else {
+
+    echo('Kuupäev on valesti sisestatud! ['.$arv2.'.'.$arv1.'.'.$aasta.']'.'<br><br>');
+
+
+
+}
+
+if (checkdate($arv1, $arv3, $aasta2)) {
+
+    echo ('Kuupäev korras! ['.$arv1.'.'.$arv3.'.'.$aasta2.']'.'<br><br>');
+
+} else {
+
+    echo('Kuupäev on valesti sisestatud!<br>');
+
+}
+
+if (checkdate($arv3, $aasta2, $aasta)) {
+
+    echo ('Kuupäev korras!<br>');
+
+} else {
+
+    echo('Kuupäev on valesti sisestatud! ['.$arv3.'.'.$arv2.'.'.$aasta.']'.'<br><br>');
+
+}
