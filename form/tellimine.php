43e634b15b4479f862286bfb447136c467d60a49
vhniii <rasmus.laane@khk.ee>
2019-01-11 18:07:01 +0200

    Vormist info töötlemine - Kõikide vorm.html andmete väärtuste töötlemine

--- form/tellimine.php
+++ form/tellimine.php
@@ -2,5 +2,9 @@
 
 
 $toode1 = $_GET['t1'];
+$toode2 = $_GET['t2'];
+$toode3 = $_GET['t3'];
 
-echo 'Toode 1: '.$toode1;
+echo 'Toode 1: '.$toode1.'<br>';
+echo 'Toode 2: '.$toode2.'<br>';
+echo 'Toode 3: '.$toode3.'<br>';
