1f5706197049fc0c674a89c50a3fb862d33be066
vhniii <rasmus.laane@khk.ee>
2019-01-11 21:51:01 +0200

    Vormist info töötlemine - Ajafunktsioonid - Ülesanne 8

--- date/index.php
+++ date/index.php
@@ -65,3 +65,108 @@ if (checkdate($arv3, $aasta2, $aasta)) {
     echo('Kuupäev on valesti sisestatud! ['.$arv3.'.'.$arv2.'.'.$aasta.']'.'<br><br>');
 
 }
+
+
+
+echo 'Ülesanne 8<br><br>';
+
+echo date('d.m.Y G:i' , time()).'<br>';
+
+$eesti_np = array(
+    'Mon' => 'Esmaspäev',
+    'Tue' => 'Teisipäev',
+    'Wed' => 'Kolmapäev',
+    'Thu' => 'Neljapäev',
+    'Fri' => 'Reede',
+    'Sat' => 'Laupäev',
+    'Sun' => 'Pühapäev'
+);
+
+$nadalapaev = $eesti_np[date('D')];
+
+
+//echo 'N:'.date('D', $eesti_np);
+echo 'N:'.$nadalapaev.'<br>';
+
+echo 'Eestikeelne kuupäev koos nädalapäevaga: '.$paev.'.'.$kuu.'.'.$aasta.', '.$nadalapaev.'<br><br>';
+
+$tana = time();
+
+//$jaanipaev = (date('d.m.Y', '24.06.2019'));
+$jaanipaev = strtotime("24 June 2019");
+
+$vahe = $jaanipaev - $tana;
+
+echo date('Y', time()).'.aasta jaanipäevani on jäänud '.round($vahe / (60*60*24)).' päeva!<br><br>';
+
+
+$metsheina_sp = mktime(0,0,0,11,06,1980);
+$minu_sp = mktime(0,0,0,06,02,1999);
+
+$metsheina_spd = date('d.m.Y', $metsheina_sp);
+$minu_spd = date('d.m.Y', $minu_sp);
+
+$vahe = ($minu_sp - $metsheina_sp);
+
+$vanusevahe = ($vahe / (60 * 60 * 24 * 365));
+
+
+
+
+echo  'Metsheina sünnikuupäev on: '.$metsheina_spd.', minu sünnikuupäev on: '.$minu_spd.'. Vanusevahe on: '.number_format($vanusevahe, 1).'<br><br>';
+
+
+if (checkdate(02,29,2016)) {
+
+    echo '29.02.2016 oleks olnud võimalik maailmalõpu saabumine!<br><br>';
+
+} else {
+
+    echo '29.02.2016 ei oleks olnud maailmalõpu saabumine võimalik!<br><br>';
+
+}
+
+
+/* Kas mul on järgmine aasta juubel? */
+
+
+$yr = date('Y', time());
+$nxtyr = $yr + 1;
+
+$vanusnxtyr = $nxtyr - date('Y', mktime(0,0,0,6,2,1999));
+
+//echo $nxtyr - date('Y', mktime(0,0,0,6,2,1999));
+
+echo 'Praegu on aasta: '.$yr.'<br>';
+echo 'Järgmine aasta on: '.$nxtyr.'<br>';
+
+
+$vanusnxtyr = (string)$vanusnxtyr;
+
+if ($vanusnxtyr[strlen($vanusnxtyr)-1] == 0 or $vanusnxtyr[strlen($vanusnxtyr)-1] == 5){
+
+    echo 'Järgmisel aastal olen ma '.$vanusnxtyr.' aastane ning mul on juubel.';
+
+} else {
+
+    echo 'Järgmisel aastal olen ma '.$vanusnxtyr.' aastane ning mul ei ole juubelit.';
+
+}
+
+echo '<br><br><br><br>';
+
+if (date('G:i', time()) > '08:00') {
+
+    echo 'Praeguse aja järgi on tervitus järgnev:   Tere hommikust!';
+
+} elseif (date('G:i', time()) > '13:00') {
+
+    echo 'Praeguse aja järgi on tervitus järgnev:   Tere päevast!';
+
+} elseif (date('G:i', time()) > '17:00') {
+
+    echo 'Praeguse aja järgi on tervitus järgnev:   Tere õhtust!';
+
+}
+
+
