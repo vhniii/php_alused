94d2637af555bba546271abdaf5859b8b3164671
vhniii <rasmus.laane@khk.ee>
2019-01-11 18:56:10 +0200

    Vormist info töötlemine - Ajafunktsioonid - Matemaatilised tehted kuupäevaga

--- date/index.php
+++ date/index.php
@@ -1,8 +1,8 @@
 <?php
 
 echo 'Time = '.time().'<br>';
-echo 'Date: '.date('d.m.Y G:i' , time()).'<br>';
-echo 'Ajavöönd: '.date_default_timezone_get('Europe/Tallinn').'<br>';
+echo 'Date: '.date('d.m.Y G:i' , time()).'<br><br>';
+echo 'Ajavöönd: '.date_default_timezone_get('Europe/Tallinn').'<br><br>';
 
 
 echo date('d.F.Y').'<br>';
@@ -17,5 +17,10 @@ $aasta = date('Y');
 echo 'Eestipärane kuupäev: '.$paev.'.'.$kuu.'.'.$aasta.'<br>';
 
 
-$a = mktime(0,0,0,10,22,1995);
-echo 'Muu soovitud kuupäeva genereerimine: '.date('d.m.Y', $a).'<br>';
+$a = mktime(0,0,0,10,22,1995-25);
+echo 'Muu soovitud kuupäeva genereerimine: '.date('d.m.Y', $a).'<br><br>';
+
+
+echo '1min pärast on kell: '.date('d.m.Y G:i', time() + 60).'<br>';
+echo '1hr pärast on kell: '.date('d.m.Y G:i', time() + 60*60).'<br>';
+echo '24hr pärast on kell: '.date('d.m.Y G:i', time() + 60*60*24).'<br>';
