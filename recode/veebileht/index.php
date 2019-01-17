e9c0f783a34d674688c21d469d47d1cf2f468e6a
vhniii <rasmus.laane@khk.ee>
2019-01-17 23:58:23 +0200

    Require/Include kasutamine - Ülesanne 10

---
+++ recode/veebileht/index.php
@@ -0,0 +1,70 @@
+<html>
+
+
+<head>
+
+    <title>Veebileht - Kontakt</title>
+
+</head>
+
+
+<body>
+
+<h1>Avaleht</h1>
+
+<div class="main-menu">
+
+    <ul>
+        <li><a href="index.php">Avaleht</a></li>
+        <li><a href="tooted.php?leht=tooted">Tooted</a></li>
+        <li><a href="hinnakiri.php?leht=hinnakiri">Hinnakiri</a></li>
+        <li><a href="kontakt.php?leht=kontakt">Kontakt</a></li>
+
+    </ul>
+
+</div>
+
+<div class="content">
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dui massa, lobortis at magna ut, venenatis ultrices libero.
+        Praesent pulvinar nisl erat, tincidunt convallis sapien aliquet at. Fusce sit amet erat lectus. Quisque interdum nisi sit amet diam ornare cursus.
+        Nunc malesuada mi leo, at aliquet purus sagittis a. Nam nisi dolor, tincidunt non erat sed, tempor tempor augue.
+        Fusce tempor lorem eu neque accumsan, vitae tempor ipsum sagittis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        Sed nec est et enim pellentesque fringilla. Sed eget lacinia arcu, sit amet pharetra tortor.
+        Morbi dolor eros, euismod eget odio dignissim, lobortis ultrices ex. Pellentesque pretium sapien ac orci dictum dictum.
+        Proin commodo nulla orci, eu viverra purus pretium vestibulum. Ut a scelerisque lectus, eu mattis ligula. Nulla at est mauris.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+    </p>
+
+</div>
+
+<?php
+
+if(!empty($_GET('leht'))) {
+
+    $page = htmlspecialchars($_GET['leht']);
+    $pages = array('tooted', 'hinnakiri', 'kontakt');
+    $check = in_array($page, $pages);
+
+    if ($check == true) {
+
+        include($page.'.php');
+
+    } else {
+
+        echo 'Sellist lehte ei eksisteeri!';
+
+    }
+
+
+}
+
+?>
+
+
+</body>
+
+
+
+</html>
