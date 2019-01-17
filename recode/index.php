25b7c9e2c58cbbd6b1df69214035b1a171f513f7
vhniii <rasmus.laane@khk.ee>
2019-01-17 23:14:32 +0200

    Require/Include kasutamine - Kontroll, kas sellist lehte on olemas?

--- recode/index.php
+++ recode/index.php
@@ -27,7 +27,15 @@
 
 if (!empty($_GET['leht'])) {
 
-    include($_GET['leht'].'.php');
+    $leht = htmlspecialchars($_GET['leht']);
+    if (is_file($leht.'.php')){
+
+        include($leht.'.php');
+
+    } else {
+
+        echo 'Lehte ei ole olemas!';
+    }
 
 }
 
