9bb8608bdeb112084c3e8ca64c4ebcf4507f33c7
vhniii <rasmus.laane@khk.ee>
2019-01-22 11:03:05 +0200

    Massiivid - Tekstitöötlus - stringi pikkus sõnades/tähemärkides

--- text/index.php
+++ text/index.php
@@ -2,6 +2,7 @@
 
 
 $text =  "Lorem ipsum dolor sit amet.";
+$text2 =  "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.";
 
 echo strtolower($text).'<br>';
 echo strtoupper($text).'<br>';
@@ -9,3 +10,9 @@ echo strtoupper($text).'<br>';
 echo '<br>';
 echo ucfirst(strtolower($text)).'<br>';
 
+
+echo '<br>';
+
+
+echo $text.' = '.strlen($text).' tähemärki.'.'<br>';
+echo $text2.' = '.str_word_count($text2).' sõna.'.'<br>';
