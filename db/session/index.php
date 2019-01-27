015ceefc306838fcb7b9e43bb41c4250ea5a6e3e
vhniii <rasmus.laane@khk.ee>
2019-01-28 00:09:38 +0200

    Andmebaas - Loodud kasutaja sessioon

--- db/session/index.php
+++ db/session/index.php
@@ -1,5 +1,14 @@
 <?php
 
+session_start();
 require_once 'fnk.php';
-$loginForm = htmlFromFile('login');
-echo $loginForm;
+
+
+
+
+if(isset($_SESSION['user'])){
+    echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+} else {
+    $loginForm = htmlFromFile('login');
+    echo $loginForm;
+}
