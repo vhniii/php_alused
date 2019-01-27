0f3172aa9a3e6a5c9f680bcb32202ac151eaac49
vhniii <rasmus.laane@khk.ee>
2019-01-27 23:46:27 +0200

    Andmebaas - Kasutaja andmete kontroll andmebaasist

--- db/login.php
+++ db/login.php
@@ -1,7 +1,16 @@
 <?php
 
+require_once 'db_func.php';
+require_once 'conf.php';
 
 $username = $_GET['uname'];
 $password = $_GET['upass'];
 
-echo $username.' '.$password;
+
+// loome ühendus andmebaasiga
+$ikt = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+// küsime kasutaja andmete olemasolu andmebaasis
+$sql = 'SELECT * FROM user WHERE username="'.$username.'" AND password="'.md5($password).'"';
+$users = getData($sql, $ikt);
+// väljastame andmed
+echo $users[0]['first_name'].' '.$users[0]['last_name'];
