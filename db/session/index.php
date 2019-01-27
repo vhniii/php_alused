9567e49a25ef23a360c720de2976340527b7e319
vhniii <rasmus.laane@khk.ee>
2019-01-28 00:14:01 +0200

    Andmebaas - loodud kasutaja sessiooni lõpetamine - välja logimine

--- db/session/index.php
+++ db/session/index.php
@@ -1,13 +1,14 @@
 <?php
 
 session_start();
-require_once 'fnk.php';
+require_once 'func.php';
 
 
 
 
 if(isset($_SESSION['user'])){
     echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+    echo '<a href="../logout.php">Logi välja</a>';
 } else {
     $loginForm = htmlFromFile('login');
     echo $loginForm;
