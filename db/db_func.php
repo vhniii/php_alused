c34f940ec57a23ac6aabdae078b7e17a2c232ef6
vhniii <rasmus.laane@khk.ee>
2019-01-27 23:08:11 +0200

    Andmebaas - Andmebaasi(st) sisestamiseks/väljastamiseks

--- db/db_func.php
+++ db/db_func.php
@@ -28,3 +28,20 @@ function connect_db ($hostName, $dbUser,  $dbPass, $dbName)
 
 
 }
+function query($sql, $conn){
+    $result = mysqli_query($conn, $sql);
+    if(!$result){
+        echo 'Problem with <b>'.$sql.'</b><br>';
+        exit;
+    }
+    return $result;
+}
+function getData($sql, $conn){
+    $data = array();
+    $result = query($sql, $conn);
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        $data[] = $row;
+    }
+    return $data;
+
+}
--- db/index.php
+++ db/index.php
@@ -6,6 +6,19 @@ require_once 'db_func.php';
 
 $iktconn = connect_db('DBHOST', 'DBUSER', 'DBPASS', 'DBNAME');
 
+// insert/update tüüpi sql testimine
+$sql = 'UPDATE user SET last_name="Ebatavaline" WHERE user_id=1';
+$res = query($sql, $iktConn);
+// select tüüpi sql testimine
+$sql = 'SELECT * FROM user';
+$users = getData($sql, $iktConn);
+
+
 echo '<pre>';
-print_r($iktconn);
+
+print_r($users);
+
 echo '</pre>';
+
+
+echo 'Tere, '.$users[0]['first_name'].' '.$users[0]['last_name'].'<br>';
