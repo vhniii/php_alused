a35086ec26a37808e88c2ce26eeb00e150818433
vhniii <rasmus.laane@khk.ee>
2019-01-28 14:02:50 +0200

    Andmebaas - funktsioon vormi väljastamiseks

---
+++ session/fnk.php
@@ -0,0 +1,10 @@
+<?php
+
+
+function htmlFromFile($fileName){
+    $fileName = 'html/'.$fileName.'.html';
+    $fp = fopen($fileName, 'r');
+    $content = fread($fp, filesize($fileName));
+    fclose($fp);
+    return $content;
+}
---
+++ session/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'fnk.php';
+$loginForm = htmlFromFile('login');
+echo $loginForm;
