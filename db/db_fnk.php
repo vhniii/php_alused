5a772c3233786ad5d852e4f61c4c3b41cc86c193
vhniii <rasmus.laane@khk.ee>
2019-01-28 14:00:26 +0200

    Andmebaas - Andmebaasi(st) andmete sisestamiseks/väljastamiseks

--- db/db_fnk.php
+++ db/db_fnk.php
@@ -26,3 +26,21 @@ function connect_db($hostName, $dbUser, $dbPass, $dbName) {
     return $conn;
 
 }
+
+function query($sql, $conn){
+    $result = mysqli_query($conn, $sql);
+    if(!$result){
+        echo 'Problem with <b>'.$sql.'</b><br>';
+        exit;
+    }
+    return $result;
+}
+function getData($sql, $conn){
+    $data = array();
+    $result = query($sql, $conn);
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        $data[] = $row;
+    }
+    return $data;
+
+}
--- db/index.php
+++ db/index.php
@@ -7,6 +7,17 @@ require_once 'db_fnk.php';
 
 $iktConn = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
 
+// insert/update tüüpi sql testimine
+$sql = 'UPDATE user SET last_name="Ebatavaline" WHERE user_id=1';
+$res = query($sql, $iktConn);
+// select tüüpi sql testimine
+$sql = 'SELECT * FROM user';
+$users = getData($sql, $iktConn);
+
 echo '<pre>';
-    print_r($iktConn);
-echo '</pre>';
+
+print_r($users);
+
+echo '<pre>';
+
+echo 'Tere, '.$users[0]['first_name'].' '.$users[0]['last_name'].'<br>';
