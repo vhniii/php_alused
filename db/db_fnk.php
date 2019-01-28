85a11978a8e556974f1206eea843a50787c4cd02
vhniii <rasmus.laane@khk.ee>
2019-01-28 13:42:26 +0200

    Andmebaasid - Loodud ühendus andmebaasiga

---
+++ db/db_fnk.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+/**
+ * @param $hostName
+ * @param $dbUser
+ * @param $dbPass
+ * @param $dbName
+ * @return mysqli
+ */
+
+function connect_db($hostName, $dbUser, $dbPass, $dbName) {
+
+    $conn = mysqli_connect($hostName, $dbUser, $dbPass, $dbName);
+
+    if(!$conn) {
+
+        echo 'Unable to connect to MySQL<br>';
+        echo mysqli_connect_error().'<br>';
+        exit;
+
+    }
+
+    return $conn;
+
+}
