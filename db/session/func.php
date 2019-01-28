49f118794c12030251e669e6c78ea1eaad10ac05
vhniii <45164906+vhniii@users.noreply.github.com>
2019-01-28 13:23:09 +0200

    Delete func.php

--- db/session/func.php
+++
@@ -1,8 +0,0 @@
-<?php
-function htmlFromFile($fileName){
-    $fileName = 'html/'.$fileName.'.html';
-    $fp = fopen($fileName, 'r');
-    $content = fread($fp, filesize($fileName));
-    fclose($fp);
-    return $content;
-}
