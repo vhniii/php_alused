f4c7bf795e569a117f1de8f3dacff90508ad04da
vhniii <rasmus.laane@khk.ee>
2019-01-28 14:13:02 +0200

    Andmebaas - väljalogimine

--- session/index.php
+++ session/index.php
@@ -7,6 +7,7 @@ require_once 'fnk.php';
 
 if(isset($_SESSION['user'])){
     echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+    echo '<a href="logout.php">Logi välja</a>';
 } else {
     $loginForm = htmlFromFile('login');
     echo $loginForm;
