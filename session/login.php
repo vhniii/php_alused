f10398f1ec7efb75045bb15fe3dd7c7dbefdba03
vhniii <rasmus.laane@khk.ee>
2019-01-28 14:11:38 +0200

    Andmebaas - kasutaja sessioon

--- session/login.php
+++ session/login.php
@@ -14,5 +14,11 @@ $ikt = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
 // küsime kasutaja andmete olemasolu andmebaasis
 $sql = 'SELECT * FROM user WHERE username="'.$username.'" AND password="'.md5($password).'"';
 $users = getData($sql, $ikt);
-// väljastame andmed
-echo $users[0]['first_name'].' '.$users[0]['last_name'];
+
+
+// loome kasutaja sessiioon, kui ta andmebaasis olemas
+if($users !== false){
+    session_start();
+    $_SESSION['user'] = $users[0];
+    header('Location: index.php');
+}
